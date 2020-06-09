3e24f6ad86ac8fef0e457b8a0545a9fd7a9fd878
maulanardy <arbomb.serv@gmail.com>
2020-06-09 23:38:16 +0700

    init commit

---
+++ app/Asosiasi.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Asosiasi extends Model
+{
+  protected $table = 'master_asosiasi';
+
+  protected $primaryKey = 'id_asosiasi';
+
+  protected $casts = ['id_asosiasi' => 'string'];
+    
+  public function sign()
+  {
+    return $this->hasMany('App\AsosiasiSign', 'asosiasi_id');
+  }
+
+  public function verifikatorSign() {
+    return $this->sign()->where('type', "verifikator");
+  }
+
+  public function databaseSign() {
+    return $this->sign()->where('type', "database");
+  }
+}
