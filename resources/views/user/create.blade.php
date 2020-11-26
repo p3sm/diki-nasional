e0753df2c964b6b9f890c87dba867e5d81ba8917
maulanardy <arbomb.serv@gmail.com>
2020-11-26 22:15:02 +0700

    update

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -66,7 +66,9 @@
                   <select class="form-control" name="asosiasi_id">
 										<option value="">-- pilih asosiasi --</option>
                     @foreach ($asosiasi as $as)
-                    <option value="{{$as->id_asosiasi}}">{{$as->nama}}</option>
+											@if($as->id_asosiasi == "148" || $as->id_asosiasi == "142")
+												<option value="{{$as->id_asosiasi}}">{{$as->nama}}</option>
+											@endif
                     @endforeach
                   </select>
                 </div>
@@ -75,9 +77,7 @@
                   <select class="form-control" name="provinsi_id">
 										<option value="">-- pilih provinsi --</option>
 										@foreach ($provinsi as $prov)
-											@if($prov->id_provinsi == "04")
-												<option value="{{$prov->id_provinsi}}" {{$prov->id_provinsi == "04" ? "selected" : ""}}>{{$prov->nama}}</option>
-											@endif
+											<option value="{{$prov->id_provinsi}}" {{$prov->id_provinsi == "04" ? "selected" : ""}}>{{$prov->nama}}</option>
                     @endforeach
                   </select>
                 </div>
